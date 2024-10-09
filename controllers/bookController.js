f1ab84297727c1ba25483d9125f012dbdc97c143
Ripul Khurana <ripulkhurana77@gmail.com>
2024-10-09 16:55:07 +0530

    success message added

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -8,7 +8,7 @@ exports.addBook = async (req, res) => {
     try {
         const { title, author } = req.body;
         const newBook = await Book.create({ title, author });
-        res.status(201).json(newBook);
+        res.status(201).json({newBook, success: true});
     } 
     catch (error) {
         res.status(500).json({ error: error.message });
